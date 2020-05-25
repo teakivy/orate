c83b5c5456b9d290a9aea5c3fa481402898340ba
teakivy <53413687+teakivy@users.noreply.github.com>
2020-05-25 19:54:27 -0400

    Add files via upload

---
+++ Contest2020/JS/main.js
@@ -0,0 +1,38 @@
+var speakStatement = ""
+
+
+var a = "a"
+var b = "b"
+var c = "c"
+var d = "d"
+var e = "e"
+var f = "f"
+var g = "g"
+var h = "h"
+var i = "i"
+var j = "j"
+var k = "k"
+var l = "l"
+var m = "m"
+var n = "n"
+var o = "o"
+var p = "p"
+var q = "q"
+var r = "r"
+var s = "s"
+var t = "t"
+var u = "u"
+var v = "v"
+var w = "w"
+var x = "x"
+var y = "y"
+var z = "z"
+var space = " "
+var period = "."
+var comma = ","
+
+
+function letter(letter) {
+  speakStatement = document.getElementById("fname").value + letter
+  document.getElementById("fname").value=speakStatement
+}
